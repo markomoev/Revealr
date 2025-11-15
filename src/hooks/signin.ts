e8fba78f73e6873b3119dcf4334c68c5c63d1180
Marko Moev <marko.moev.business@gmail.com>
2025-11-15 19:18:58 +0200

    hook created

--- src/hooks/signin.ts
+++ src/hooks/signin.ts
@@ -1,8 +1,18 @@
 import {supabase} from '../client'
 
-export const signInUser = () => {
+export const signInUser = async (email: string, password: string) => {
     try{
+        const { data, error: signinUserError } = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        })
 
+        if(signinUserError){
+            console.error('Error in signing in! ' + signinUserError.message )
+            return;
+        }
+
+        return data;
     }
     catch(error){
         console.error('An unexpected error occured!')
